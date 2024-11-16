7519bcb20741d8f17e8ce3ef64c1ec9f565e8b58
GabrielSiqueira98 <gabrieldossantossiqueira@gmail.com>
2024-11-16 15:36:53 -0300

    ultimas atualizações

--- App.tsx
+++ App.tsx
@@ -1,47 +1,69 @@
 import React from 'react';
-import { View, Text, ScrollView, StyleSheet} from 'react-native';
-import { Exercicios } from "./src/components/Exercicios";
-
+import { View, Text, ScrollView, StyleSheet } from 'react-native';
+import { Exercicios } from './src/components/Exercicios';
+import { MaterialCommunityIcons, FontAwesome, FontAwesome5, FontAwesome6, Ionicons, Foundation } from '@expo/vector-icons';
 
 export function App() {
   return (
     <View style={styles.container}>
       <View style={styles.header}>
-        <Text style={styles.title}>TREINO EM CASA</Text>
+        <Text style={styles.titulo}>TREINO EM CASA</Text>
       </View>
 
-      <View style={styles.levels}>
-        <Text style={styles.levelActive}>Iniciante</Text>
-        <Text style={styles.level}>Intermediário</Text>
-        <Text style={styles.level}>Avançado</Text>
+      <View style={styles.modulos}>
+        <Text style={styles.moduloSelecionado}>Iniciante</Text>
+        <View style={styles.selecionado} />
+        <Text style={styles.modulo}>Intermediário</Text>
+        <Text style={styles.modulo}>Avançado</Text>
       </View>
-
-      <ScrollView>
+      
+      {/* <ScrollView style={styles.scrollContainer}> */}
         <Exercicios
-       titulo="ABDÔMEN INICIANTE"
-       duracao="20 MIN"
-       exercicios="16"
-       imagem={require('./assets/ABDOMEN.jpeg')} 
-     />
-         <Exercicios
+          titulo="ABDÔMEN INICIANTE"
+          duracao="20 MIN"
+          exercicios="16"
+          imagem={require('./assets/ABDOMEN.jpeg')}
+        /> 
+        <Exercicios
           titulo="PEITO INICIANTE"
           duracao="7 MIN"
           exercicios="11"
           imagem={require('./assets/PEITO.jpg')}
         />
-         <Exercicios
+        <Exercicios
           titulo="BRAÇO INICIANTE"
           duracao="17 MIN"
           exercicios="19"
           imagem={require('./assets/BRACO.jpg')}
         />
-           <Exercicios
+        <Exercicios
           titulo="PERNA INICIANTE"
           duracao="26 MIN"
           exercicios="23"
-          imagem={require('./assets/PERNA.jpg')} 
+          imagem={require('./assets/PERNA.jpg')}
         />
-      </ScrollView>
+      
+      {/* </ScrollView> */}
+      <View style={styles.footer}>
+        <View style={styles.footerIcone}>
+          <MaterialCommunityIcons name="clock" size={24} color="blue" />
+          <Text style={styles.footermoduloTreino}>Treino</Text>
+          <View style={styles.selecaoTreino} />
+        </View>
+        <View style={styles.footerIcone}>
+          <Ionicons name="compass" size={24} color="gray" />
+          <Text style={styles.footerTexto}>Descobrir</Text>
+        </View>
+        <View style={styles.footerIcone}>
+          <Foundation name="graph-bar" size={24} color="gray" />
+          <Text style={styles.footerTexto}>Relatório</Text>
+        </View>
+        <View style={styles.footerIcone}>
+          <MaterialCommunityIcons name="account" size={24} color="gray" />
+          <Text style={styles.footerTexto}>Definição</Text>
+          <View style={styles.notificacao} />
+        </View>
+      </View>
     </View>
   );
 }
@@ -49,35 +71,85 @@ export function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#F5F5F5',
     paddingHorizontal: 20,
     paddingTop: 50,
+    top:-20,
   },
   header: {
     flexDirection: 'row',
     alignItems: 'center',
     justifyContent: 'space-between',
   },
-  title: {
+  titulo: {
     fontSize: 24,
     fontWeight: 'bold',
+
   },
-  icon: {
-    width: 24,
-    height: 24,
-  },
-  levels: {
+  modulos: {
     flexDirection: 'row',
     justifyContent: 'space-around',
     marginVertical: 20,
+    top: -5,
   },
-  levelActive: {
+  moduloSelecionado: {
     fontSize: 16,
     fontWeight: 'bold',
     color: 'black',
   },
-  level: {
+  modulo: {
     fontSize: 16,
     color: 'gray',
+    fontWeight: 'bold'
+  },
+  selecionado: {
+    position: 'absolute',
+    top: 22,
+    left: 19,
+    width: 27,
+    height: 5,
+    backgroundColor: 'blue',
+    borderRadius: 4,
+  },
+  scrollContainer: {
+    flex: 1,
+    top: -12,
+  },
+  footer: {
+    height: 30,
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    alignItems: 'center',
+  },
+  footerIcone: {
+    alignItems: 'center',
+  },
+  footerTexto: {
+    fontSize: 12,
+    color: 'gray',
+    fontWeight:'bold',
+  },
+
+  footermoduloTreino: {
+    fontSize: 12,
+    color:'blue',
+    fontWeight:'bold',
+  },
+  selecaoTreino: {
+    position: 'absolute',
+    top: 0,
+    left: 14,
+    width: 5,
+    height: 2,
+    backgroundColor: 'blue',
+    borderRadius: 4,
+  },
+  notificacao: {
+    position: 'absolute',
+    top: -2,
+    right: 6,
+    width: 8,
+    height: 8,
+    backgroundColor: 'red',
+    borderRadius: 4,
   },
 });
--- src/components/Exercicios.tsx
+++ src/components/Exercicios.tsx
@@ -1,15 +1,19 @@
 import React from 'react';
-import { View, Text, ImageBackground, StyleSheet} from 'react-native';
+import { View, Text, ImageBackground, StyleSheet } from 'react-native';
 import { ExerciciosProps } from '../interface/interface';
-
-
+import { MaterialCommunityIcons } from '@expo/vector-icons';
 
 export function Exercicios({ titulo, duracao, exercicios, imagem }: ExerciciosProps) {
   return (
-    <ImageBackground source={imagem} style={styles.card} imageStyle={styles.cardImage}>
-      <View style={styles.overlay}>
-        <Text style={styles.title}>{titulo}</Text>
-        <Text style={styles.info}>{duracao} • {exercicios} EXERCÍCIOS</Text>
+    <ImageBackground source={imagem} style={styles.card} imageStyle={styles.ImagemCard}>
+      <View style={styles.iconeContainer}>
+        <MaterialCommunityIcons name="lightning-bolt" size={16} color="blue" />
+        <MaterialCommunityIcons name="lightning-bolt" size={16} color="gray" />
+        <MaterialCommunityIcons name="lightning-bolt" size={16} color="gray" />
+      </View>
+      <View style={styles.filtro}>
+        <Text style={styles.titulo}>{titulo}</Text>
+        <Text style={styles.informacoes}>{duracao} • {exercicios} EXERCÍCIOS</Text>
       </View>
     </ImageBackground>
   );
@@ -18,27 +22,35 @@ export function Exercicios({ titulo, duracao, exercicios, imagem }: ExerciciosPr
 const styles = StyleSheet.create({
   card: {
     height: 150,
-    borderRadius: 10,
+    borderRadius: 15,
     overflow: 'hidden',
-    marginVertical: 10,
+    marginVertical: 5,
+    top:-10,
   },
-  cardImage: {
+  ImagemCard: {
     borderRadius: 10,
   },
-  overlay: {
+  filtro: {
     flex: 1,
     backgroundColor: 'rgba(0, 0, 0, 0.5)',
     justifyContent: 'center',
     padding: 10,
   },
-  title: {
+  titulo: {
     color: '#FFF',
     fontSize: 18,
     fontWeight: 'bold',
   },
-  info: {
+  informacoes: {
     color: '#FFF',
     fontSize: 14,
     marginTop: 5,
+  },
+  iconeContainer: {
+    position: 'absolute',
+    top: 12, 
+    left: 10, 
+    flexDirection: 'row', 
+    alignItems: 'center', 
   },
 });
